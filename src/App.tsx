f168afd41ae48ea068c170926dd2f8bdfcf342f6
Esto Triramdani N <estolagi@gmail.com>
2022-11-17 22:11:04 +0700

    multiple canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [listId, setListId] = useState<string[]>([]);
   useEffect(() => {
     const myCanvas = document.getElementById('mycanvas')! as HTMLCanvasElement;
     const ctx = myCanvas.getContext('2d')!;
@@ -17,14 +18,58 @@ const App = () => {
     ctx.clearRect(0, 0, 50, 50);
   };
 
+  const removeCanvas = (id: string) => {
+    document.getElementById(id)!.remove();
+    setListId((prev) => prev.filter((item) => item !== id));
+  };
+
+  const createNewCanvas = () => {
+    const newDiv = document.createElement('canvas') as HTMLCanvasElement;
+    const id = Math.random().toString();
+    newDiv.id = id;
+    setListId((prev) => [...prev, id]);
+    newDiv.style.height = '80vh';
+    newDiv.style.width = '50vw';
+    newDiv.style.position = 'absolute';
+    newDiv.style.top = '0';
+    newDiv.style.left = '0';
+    const parentElement = document.getElementById(
+      'parentElement'
+    )! as HTMLDivElement;
+    parentElement.appendChild(newDiv);
+    const ctx = newDiv.getContext('2d')!;
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'blue';
+    ctx.rect(100 * Math.random(), 100 * Math.random(), 200, 200);
+    ctx.fill();
+    ctx.stroke();
+  };
+
   return (
-    <div>
+    <div
+      id="parentElement"
+      className="relative"
+      style={{ width: '50vw', height: '80vh' }}
+    >
       <canvas
         id="mycanvas"
         style={{ height: '80vh', width: '50vw' }}
-        className="border"
+        className="border absolute top-0 left-0"
       ></canvas>
-      <button>clear</button>
+      <button onClick={createNewCanvas} className="fixed right-0">
+        new canvas
+      </button>
+      <div className="fixed bottom-0 right-0">
+        <ul>
+          {listId.map((item) => (
+            <li key={item}>
+              <button onClick={() => removeCanvas(item)}>
+                Remove Item {item}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
