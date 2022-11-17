1afc0b1caeb72c1aa4bece5741c765b2980e5bda
Esto Triramdani N <estolagi@gmail.com>
2022-11-17 22:15:10 +0700

    add todo

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,14 @@ const App = () => {
     const ctx = myCanvas.getContext('2d')!;
     ctx.clearRect(0, 0, 50, 50);
   };
-
+  /* // TODO
+    1 ATUR Z-INDEX PER Canvas (listId ditambah property zIndex)
+    2 custom color (background and stroke)
+    3 insert rect
+    4 insert arc
+    5 insert image
+    6 drag canvas image
+  */
   const removeCanvas = (id: string) => {
     document.getElementById(id)!.remove();
     setListId((prev) => prev.filter((item) => item !== id));
